c21c27a7c99e8e0320841dc8204270b5efef4ee8
AlexNadimyan <alexnadimyandev@gmail.com>
2024-07-11 17:09:52 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+console.log(23 + 45);
+console.log(104 - 63);
+console.log(16 / 8);
+console.log(0.7 / 8);
+console.log(6 % 4);
+console.log((50 - 60) / 2);
+
+console.log(5 > 6);
+console.log(4 < 7);
+console.log(7 >= 10);
+console.log(8 <= 8);
+console.log(101 == '101');
+console.log(101 === '101');
+console.log(!true);
+console.log(!false);
+console.log(101 != '101');
+console.log(101 !== '101');
+
+console.log(5 > 8 && 6 === 6);
+
+console.log(10 > 2 || 5 == '5');
