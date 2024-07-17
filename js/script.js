a12271c4d306f4ea1600638b2f15eec5be09b07e
AlexNadimyan <alexnadimyandev@gmail.com>
2024-07-17 14:11:36 +0300

    film-app with lopps and conditions

--- js/script.js
+++ js/script.js
@@ -10,14 +10,28 @@ const DB = {
   privat: false
 };
 
-const firstFilm = prompt("What movie did you watch recently?");
-const firstFilmGrade = +prompt("How much do you rate that movie?");
-const secondFilm = prompt("What movie did you watch recently?");
-const secondFilmGrade = +prompt("How much do you rate that movie?");
-const thirdFilm = prompt("What movie did you watch recently?");
-const thirdFilmGrade = +prompt("How much do you rate that movie?");
 
-DB.movies[firstFilm] = firstFilmGrade;
-DB.movies[secondFilm] = secondFilmGrade;
 
+for (let i = 0; i < 2; i++) {
+  const filmName = prompt("What movie did you watch recently?");
+  const filmRate = +prompt(`How much do you rate ${filmName}?`);
+
+  if (filmName != null && filmRate != null && filmName != "" && filmRate != "" && filmName.length < 50) {
+    DB.movies[filmName] = filmRate;
+    console.log("Resolve");
+  } else {
+    console.log("Reject");
+    i--;
+  }
+}
+
+if (DB.count < 10) {
+  DB.status = "Դուք դիտել եք բավականին քիչ ֆիլմեր";
+} else if (DB.count >= 10 && DB.count < 30) {
+  DB.status = "Դուք դասական ֆիլմ դիտող եք";
+} else if (DB.count >= 30) {
+  DB.status = "Դուք կինոման եք !"
+} else {
+  console.log("Տեղի է ունեցել խնդիր․");
+}
 console.log(DB);
