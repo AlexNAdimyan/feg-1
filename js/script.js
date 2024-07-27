322902c2aa9d940967b4e4dc445fd1004d4f2284
AlexNadimyan <alexnadimyandev@gmail.com>
2024-07-27 16:08:24 +0300

    on film-app we added function

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 "use strict";
 
-const countOfFilms = +prompt("How many movies have you watched ever?");
+let countOfFilms;
+
+init();
 
 const DB = {
   count: countOfFilms,
@@ -10,28 +12,60 @@ const DB = {
   privat: false
 };
 
+function init() {
+  countOfFilms = +prompt("Քանի՞ ֆիլմ եք վերջերս դիտել");
 
-
-for (let i = 0; i < 2; i++) {
-  const filmName = prompt("What movie did you watch recently?");
-  const filmRate = +prompt(`How much do you rate ${filmName}?`);
-
-  if (filmName != null && filmRate != null && filmName != "" && filmRate != "" && filmName.length < 50) {
-    DB.movies[filmName] = filmRate;
-    console.log("Resolve");
-  } else {
-    console.log("Reject");
-    i--;
+  while (countOfFilms == "" || countOfFilms == null || isNaN(countOfFilms)) {
+    countOfFilms = +prompt("Քանի՞ ֆիլմ եք վերջերս դիտել");
   }
 }
 
-if (DB.count < 10) {
-  DB.status = "Դուք դիտել եք բավականին քիչ ֆիլմեր";
-} else if (DB.count >= 10 && DB.count < 30) {
-  DB.status = "Դուք դասական ֆիլմ դիտող եք";
-} else if (DB.count >= 30) {
-  DB.status = "Դուք կինոման եք !"
-} else {
-  console.log("Տեղի է ունեցել խնդիր․");
+function rememberAndCreateUserFilms() {
+  for (let i = 0; i < 2; i++) {
+    const filmName = prompt("Ո՞ր ֆիլմն եք վերջերս դիտել");
+    const filmRate = +prompt(`Ինչքա՞ն եք գնահատում ${filmName} ֆիլմը`);
+
+    if (filmName != null && filmRate != null && filmName != "" && filmRate != "" && filmName.length < 50) {
+      DB.movies[filmName] = filmRate;
+      console.log("Resolve");
+    } else {
+      console.log("Reject");
+      i--;
+    }
+  }
 }
-console.log(DB);
+
+function detectUserPersonalLevel() {
+  if (DB.count < 10) {
+    DB.status = "Դուք դիտել եք բավականին քիչ ֆիլմեր";
+  } else if (DB.count >= 10 && DB.count < 30) {
+    DB.status = "Դուք դասական ֆիլմ դիտող եք";
+  } else if (DB.count >= 30) {
+    DB.status = "Դուք կինոման եք !"
+  } else {
+    console.log("Տեղի է ունեցել խնդիր․");
+  }
+}
+function userFavoriteGenres(){
+  for (let i = 1; i < 4; i++) {
+    const filmGenres = prompt(`Որն է ձեր ամենասիրած ${i} ֆիլմի ժանրը`);
+    
+    
+    if (filmGenres != null && filmGenres != "" && filmGenres.length < 50) {
+      DB.movies[filmGenres] = filmGenres;
+      console.log("Resolve");
+    } else {
+      console.log("Reject");
+      i--;
+    }
+  }
+}
+
+function checkDBPrivatStatus(state) {
+  state ? console.log("Sorry but DB is not visible") : console.log(DB);
+}
+
+userFavoriteGenres()
+rememberAndCreateUserFilms();
+detectUserPersonalLevel();
+checkDBPrivatStatus(DB.privat);
